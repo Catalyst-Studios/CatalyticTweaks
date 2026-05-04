3e3685014557d92915841984f0e6b0415b6d8621
Titop54 <32750608+Titop54@users.noreply.github.com>
2026-05-04 09:55:07 +0200

    Bump version
    Not using RandomSource

--- src/main/java/com/catalytictweaks/catalytictweaksmod/mixin/mmr/RecipeRequirementMixin.java
+++ src/main/java/com/catalytictweaks/catalytictweaksmod/mixin/mmr/RecipeRequirementMixin.java
@@ -1,12 +1,16 @@
 package com.catalytictweaks.catalytictweaksmod.mixin.mmr;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.checkerframework.checker.units.qual.C;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 import com.catalytictweaks.catalytictweaksmod.mmr.IComponentManager;
 import com.catalytictweaks.catalytictweaksmod.mmr.IRecipeRequirement;
@@ -17,6 +21,7 @@ import es.degrassi.mmreborn.api.crafting.requirement.RecipeRequirement;
 import es.degrassi.mmreborn.common.machine.IOType;
 import es.degrassi.mmreborn.common.machine.MachineComponent;
 import es.degrassi.mmreborn.common.manager.ComponentManager;
+import net.minecraft.util.RandomSource;
 
 @Pseudo
 @SuppressWarnings("hiding")
@@ -25,6 +30,8 @@ public abstract class RecipeRequirementMixin<C extends MachineComponent<T>, R ex
 {
 
     @Shadow @Final private R requirement;
+    @Shadow private float chance;
+    @Unique @Final Random rand = new Random();
 
     @Override
     public List<C> findComponents(ComponentManager manager, ICraftingContext context)
@@ -36,4 +43,12 @@ public abstract class RecipeRequirementMixin<C extends MachineComponent<T>, R ex
         }
         return List.of();
     }
+
+    @Overwrite
+    public boolean shouldSkip(RandomSource rand, ICraftingContext context)
+    {
+        float modifiedChance = context.getModifiedValue(this.chance, this.requirement);
+        return ThreadLocalRandom.current().nextFloat() > modifiedChance;
+    }
+
 }
