79f5122824542e944b0686df51370f2731c17326
Titop54 <32750608+Titop54@users.noreply.github.com>
2026-07-09 18:13:54 +0200

    Higher prio

--- src/main/java/com/catalytictweaks/catalytictweaksmod/mixin/mmr/EnergyHatchMixin.java
+++ src/main/java/com/catalytictweaks/catalytictweaksmod/mixin/mmr/EnergyHatchMixin.java
@@ -18,7 +18,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Pseudo
-@Mixin(value = EnergyHatchEntity.class, remap = false)
+@Mixin(value = EnergyHatchEntity.class, remap = false, priority = 999)
 public abstract class EnergyHatchMixin extends BlockEntitySynchronized
 {
 
